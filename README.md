c90b05687947323c8b407d70178a1857dfc6ca68
Dino Paskvan <dpaskvan@gmail.com>
2016-10-04 02:22:38 +0200

    Version 0.3.0 — added support for named events, custom event IDs

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
-#event-stream
+event-stream
+============
 
 [![NPM](https://nodei.co/npm/express-sse.png?downloads=true)](https://nodei.co/npm/express-sse/)
 
 An Express middleware for quick'n'easy server-sent events.
 
-##Installation:
+## About
+`express-sse` is meant to keep things simple. You need to send server-sent events without too many complications and fallbacks? This is the library to do so.
+
+## Installation:
 `npm install express-sse`
 
-##Usage example:
-###Server:
+## Usage example:
+### Server:
 ```
 var SSE = require('express-sse');
 var sse = new SSE(["array", "containing", "initial", "content", "(optional)"]);
@@ -20,14 +24,20 @@ app.get('/stream', sse.init);
 ...
 
 sse.send(content);
+sse.send(content, eventName);
+sse.send(content, eventName, customID);
 sse.updateInit(["array", "containing", "new", "content"]);
 ```
 
-###Client:
+### Client:
 ```
 var es = new EventSource('/stream');
 
 es.onmessage = function (event) {
   ...
 };
+
+es.addEventListener(eventName, function (event) {
+  ...
+});
 ```
--- index.js
+++ index.js
@@ -2,67 +2,73 @@
  * Require the module dependencies
  */
 
-var EventEmitter = require('events').EventEmitter;
-var util = require('util');
+const EventEmitter = require('events').EventEmitter;
 
 /**
- * Creates a new Server-Sent Event instance
- * @param {Array} initial Initial value(s) to be served through SSE (optional)
+ * Server-Sent Event instance class
+ * @extends EventEmitter
  */
+class SSE extends EventEmitter {
+  /**
+   * Creates a new Server-Sent Event instance
+   * @param [array] initial Initial value(s) to be served through SSE
+   */
+  constructor(initial = []) {
+    super();
 
-var SSE = function(initial) {
-  this.initial = initial ? initial : [];
+    this.initial = initial;
 
-  EventEmitter.call(this);
+    this.init = this.init.bind(this);
+  }
 
   /**
    * The SSE route handler
    */
-
-  this.init = function(req, res) {
-    var id = 0;
+  init(req, res) {
+    let id = 0;
     req.socket.setTimeout(Number.MAX_SAFE_INTEGER || Infinity);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/event-stream');
     res.setHeader('Cache-Control', 'no-cache');
     res.setHeader('Connection', 'keep-alive');
     if (this.initial) {
-      for (var i = 0; i < this.initial.length; i++) {
-        res.write("id: " + id + "\n");
-        res.write("data: " + JSON.stringify(this.initial[i]) + "\n\n");
+      for (let i = 0; i < this.initial.length; i++) {
+        res.write(`id: ${id}\n`);
+        res.write(`data: ${JSON.stringify(this.initial[i])}\n\n`);
         id += 1;
       }
     }
-    this.on('data', function(data) {
-      res.write("id: " + id + "\n");
-      res.write("data: " + JSON.stringify(data) + "\n\n");
-      id += 1;
+    this.on('data', data => {
+      if (data.id) {
+        res.write(`id: ${data.id}\n`);
+      } else {
+        res.write(`id: ${id}\n`);
+        id += 1;
+      }
+      if (data.event) {
+        res.write(`event: ${data.event}\n`);
+      }
+      res.write(`data: ${JSON.stringify(data.data)}\n\n`);
     });
-  }.bind(this);
+  }
 
   /**
    * Update the data initially served by the SSE stream
-   * @param {Array} data Array containing data to be served on new connections
+   * @param {array} data array containing data to be served on new connections
    */
-
-  this.updateInit = function(data) {
+  updateInit(data) {
     this.initial = data;
-  }.bind(this);
+  }
 
   /**
    * Send data to the SSE
-   * @param {Object} data Data to send into the stream
+   * @param {(object|string)} data Data to send into the stream
+   * @param [string] event Event name
+   * @param [(string|number)] id Custom event ID
    */
-
-  this.send = function(data) {
-    this.emit('data', data);
-  }.bind(this);
+  send(data, event, id) {
+    this.emit('data', {data, event, id});
+  }
 }
-
-/**
- * Inherit from EventEmitter
- */
-
-util.inherits(SSE, EventEmitter);
 
 module.exports = SSE;
