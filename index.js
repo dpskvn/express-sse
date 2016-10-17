45096536b725942fc943ce5f60d7bb9b959be986
Sean Corrales <scorrales@usft.com>
2016-10-17 09:53:15 -0500

    Adding support for autoqueuing.

--- index.js
+++ index.js
@@ -14,8 +14,13 @@ class SSE extends EventEmitter {
    * @param [array] initial Initial value(s) to be served through SSE
    * @param [object] options Options for SSE.
    */
-  constructor(initial = [], options = { isQueue: true }) {
+  constructor(initial = [], options = { isQueue: true, autoQueue: false, queueLength: 10 }) {
     super();
+
+    if(options.autoQueue) {
+      options.isQueue = true;
+      options.queueLength = typeof options.queueLength !== 'undefined' ? options.queueLength : 10;
+    }
 
     this.initial = Array.isArray(initial) ? initial : [initial];
     this.options = options;
@@ -45,16 +50,22 @@ class SSE extends EventEmitter {
         res.write(`event: ${data.event}\n`);
       }
       res.write(`data: ${JSON.stringify(data.data)}\n\n`);
+      if(!data.isInitial && this.options.autoQueue) {
+        this.initial.push(data.data);
+        if(this.options.queueLength > 0 && this.initial.length > this.options.queueLength) {
+          this.initial.splice(0, this.initial.length - this.options.queueLength);
+        }
+      }
     });
 
     if (this.initial) {
       if(this.options.isQueue) {
         id = this.initial.reduce((msgId, data) => {
-          this.send(data, null, msgId);
+          this.send(data, null, msgId, true);
           return msgId + 1;
         }, id);
       } else {
-        this.send(this.initial, null, id);
+        this.send(this.initial, null, id, true);
         id += 1;
       }
     }
@@ -74,8 +85,8 @@ class SSE extends EventEmitter {
    * @param [string] event Event name
    * @param [(string|number)] id Custom event ID
    */
-  send(data, event, id) {
-    this.emit('data', {data, event, id});
+  send(data, event, id, isInitial) {
+    this.emit('data', {data, event, id, isInitial});
   }
 }
 
